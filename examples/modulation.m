d10d55a8d235aab16bf198d64afd28a14b054bc0
Binot Emile <emile.binot@insa-rennes.fr>
2022-02-09 13:21:13 +0100

    modulations experiments

--- examples/modulation.m
+++ examples/modulation.m
@@ -1,32 +1,41 @@
 clear;
-close all;
+% close all;
 CR=3;     % Coding rate : {1,4}
 SF=7;     % Coding rate  : {7,12}
 B=125e3;  % Bandwidth : [125 kHz,250 kHz,500 kHz]
 
-symbols=[50];
+symbols=[0];
 
-txSig = LoRa_Modulation(SF,symbols,1);
-rxSig=txSig;
-
-chirp = LoRa_Modulation(SF,symbols,-1);
-
-demodSig = rxSig.*chirp;
-
-%% Plotting modulated signal
+% txSig = LoRa_Modulation(SF,symbols,1);
+% rxSig=txSig;
+% 
+% chirp = LoRa_Modulation(SF,symbols,-1);
+% 
+% demodSig = rxSig.*chirp;
 Fs = B;     % Sampling frequency
+
+txSig=LoRa_Modulation_Old(B,SF,symbols,1,Fs);
+chirp=LoRa_Modulation_Old(B,SF,zeros(1,length(symbols)),-1,Fs);
+demodSig = txSig.*chirp;
+
 Ts = 1/Fs;  % Sampling period
 ts = (0:Ts:(length(txSig)*Ts)-Ts)';
-figure;
-plot(ts,real(txSig));
 
-figure;
-subplot(3,1,1);
-spectrogram(txSig,20,15,-B/2:1e3:B/2,B,'yaxis');    % no idea how it's working
-title('txSig');
-subplot(3,1,2);
-spectrogram(txSig,20,15,128,B,'yaxis');
-title('demod chirp')  
-subplot(3,1,3);
-spectrogram(demodSig,20,15,128,B,'yaxis');
-title('txSig * demod chirp')  
+f = -Fs/2:Fs/length(txSig):Fs/2-Fs/length(txSig);
+
+Y = fftshift(fft(txSig));
+% figure;
+plot(f,abs(Y));
+% figure;
+% plot(ts,real(txSig),ts,imag(txSig));
+
+% figure;
+% subplot(3,1,1);
+% spectrogram(txSig,20,15,-B/2:1e3:B/2,Fs,'yaxis');    % no idea how it's working
+% title('txSig');
+% subplot(3,1,2);
+% spectrogram(txSig,20,15,-B/2:1e3:B/2,B,'yaxis');
+% title('demod chirp')  
+% subplot(3,1,3);
+% spectrogram(demodSig,20,15,128,B,'yaxis');
+% title('txSig * demod chirp')  
--- playground_lora/Experiment_script.m
+++ playground_lora/Experiment_script.m
@@ -14,9 +14,9 @@ tic
 
 CR=3;     % Coding rate : {1,4}
 SF=12;     % Coding rate  : {7,12}
-B=500e3;  % Bandwidth : [125 kHz,250 kHz,500 kHz]
+B=125e3;  % Bandwidth : [125 kHz,250 kHz,500 kHz]
 Pr_len=4; % Preamble length
-load("symbols","modSymbK","demodChirp");
+% load("symbols","modSymbK","demodChirp");
 load("noise","whiteNoise");
 binary_data=zeros(5,1);
 binary_data(:,2:8) = de2bi([2,50,40,90,120]);
@@ -24,17 +24,17 @@ binary_data = binary_data(:);
 
 %% LoRa Emitter
 
-[txSig,dataIn]=LoRa_Emitter(CR,SF,Pr_len,binary_data,whiteNoise); 
+[txSig,dataIn]=LoRa_Emitter(CR,SF,Pr_len,binary_data,whiteNoise,0); 
 
 %% Channel
 
 %rxSig=txSig;% Neutral
 
 rxSig=[zeros(0,1); txSig];% Neutral
-rxSig=awgn(rxSig,-20,'measured');
+rxSig=awgn(rxSig,20,'measured');
 
 %% LoRa Receiver
-[dataOut,chirp,demodSig]=LoRa_Receiver_Sync(CR,SF,B,Pr_len,rxSig,whiteNoise);
+[dataOut,chirp,demodSig]=LoRa_Receiver_Sync(CR,SF,B,Pr_len,rxSig,whiteNoise,0);
 %timer end
 toc
 % 
@@ -43,19 +43,19 @@ Fs = B;     % Sampling frequency
 Ts = 1/Fs;  % Sampling period
 ts = (0:Ts:(length(txSig)*Ts)-Ts)';
 
-figure;
-subplot(3,1,1);
-spectrogram(txSig,20,15,128,B,'yaxis');    % no idea how it's working
-title('txSig')  
-subplot(3,1,2);
-spectrogram(rxSig,20,15,128,B,'yaxis');    % no idea how it's working
-title('demod chirp')  
-subplot(3,1,3);
-spectrogram(demodSig,20,15,128,B,'yaxis');    % no idea how it's working
-title('txSig * demod chirp')  
+% figure;
+% subplot(3,1,1);
+% spectrogram(txSig,20,15,128,B,'yaxis');    % no idea how it's working
+% title('txSig')  
+% subplot(3,1,2);
+% spectrogram(chirp,20,15,128,B,'yaxis');    % no idea how it's working
+% title('demod chirp')  
+% subplot(3,1,3);
+% spectrogram(demodSig,20,15,128,B,'yaxis');    % no idea how it's working
+% title('txSig * demod chirp')  
 
-%figure;
-%plot(ts,real(rxSig),ts,real(txSig));
+figure;
+spectrogram(txSig,20,15,128,B,'yaxis');
 
 [~,ber] = biterr(dataIn,dataOut);
 disp(['BER : ' num2str(ber)])
